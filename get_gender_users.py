0722ddd25de048c3966c880b18c0959bea78a2a5
Aktam <94_aktam@mail.ru>
2022-04-20 12:59:57 +0500

    4

--- get_gender_users.py
+++ get_gender_users.py
@@ -1,4 +1,32 @@
-import get_data
+# import get_data
+
+# def get_gender_users(data:dict) -> list:
+#     """
+#     Take the gender of the users and return the list.
+#      The list items are as follows:
+#         If the user is male: {"Male":1}
+#         If the user is female: {"Female":0}
+#     Args:
+#         data(dict): data
+#     Returns:
+#         list: users get gender list
+#     """
+#     a=[]
+#     b={"Male":0 ,"Female":0 }
+#     data=data['results']
+#     for i in data :       
+#         a.append(i["gender"])
+   
+#     b["Male"]=a.count("male")
+#     b["Female"]=a.count("female")
+   
+#     return  b
+# import json
+# dat=open("randomuser_data.json").read()
+# data=json.loads(dat)   
+# print(get_gender_users(data))
+
+import json
 
 def get_gender_users(data:dict) -> list:
     """
@@ -13,20 +41,17 @@ def get_gender_users(data:dict) -> list:
     Returns:
         list: users get gender list
     """
-    a=[]
-    b={"Male":0 ,"Female":0 }
-    data=data['results']
-    for i in data :       
-        a.append(i["gender"])
-        
-    b["Male"]=a.count("male")
-    b["Female"]=a.count("female")
+    gender_list=[]
+    for i in data['results']:
+        if i['gender']=='male':
+            gender_list.append({"Male":1})
+        else:
+            gender_list.append({"Female":0})
+    return gender_list
     
-    
-    
-    return  b
 
-import json
-dat=open("randomuser_data.json").read()
-data=json.loads(dat)   
+
+f=open('./randomuser_data.json')
+data=json.load(f)
 print(get_gender_users(data))
+f.close()
