2a16f5166602f95c4e34e47e383f831715a71771
Aktam <94_aktam@mail.ru>
2022-04-21 07:11:52 +0500

    5

--- get_users_data.py
+++ get_users_data.py
@@ -1,5 +1,5 @@
 import get_data
-
+from pprint import pprint
 def get_users_data(data:dict) -> list:
     """
     Take the data of the first name, last name and phone number. Return the list.
@@ -12,4 +12,19 @@ def get_users_data(data:dict) -> list:
     Returns:
         list: users data list
     """
-    
+   
+    foydalanuvchilar_haqida_malumotlar=[]
+   
+    foydalanuvchilar=data['results']
+    for user in foydalanuvchilar :
+        ismlar=user["name"]["first"]
+        familiyalar=user["name"]["last"]
+        telefonlar=user["phone"]
+        malumotlar={"first_name" :ismlar,"last_name" :familiyalar , "phone_number" :telefonlar  }
+        foydalanuvchilar_haqida_malumotlar.append(malumotlar)
+    return foydalanuvchilar_haqida_malumotlar
+
+import json
+dat=open("randomuser_data.json").read()
+data=json.loads(dat)   
+pprint(get_users_data(data))
